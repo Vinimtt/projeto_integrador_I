aba165395ea3d316b2405655e6e5d577ccc960b7
pablo felipe <pabloletiane@gmail.com>
2025-10-08 06:13:41 -0300

    Adiciona componente NavBar com estilo em módulo CSS

--- projeto/src/app/page.js
+++ projeto/src/app/page.js
@@ -1,8 +1,10 @@
-import Image from "next/image";
+import React from 'react'
+import NavBar from '../components/NavBar'
 
 export default function Home() {
   return (
     <>
+    <NavBar/>
     
     
     </>
--- projeto/src/components/NavBar.jsx
+++ projeto/src/components/NavBar.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './NavBar.css'
+import React from 'react';
+
+import styles from './Navbar.module.css';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <>
+      
+      <nav className={styles.navbar}>
+        <div className={styles.navbarContainer}>
+          <a href="#" className={styles.navbarLogo}>MyLogo</a>
+          <ul className={styles.navbarMenu}>
+            <li className={styles.navbarItem}><a href="#" className={styles.navbarLink}>Início</a></li>
+            <li className={styles.navbarItem}><a href="#" className={styles.navbarLink}>Quem somos?</a></li>
+            <li className={styles.navbarItem}><a href="#" className={styles.navbarLink}>Serviços</a></li>
+            <li className={styles.navbarItem}><a href="#" className={styles.navbarLink}>Parceiros</a></li>
+            <li className={styles.navbarItem}><a href="#" className={styles.navbarLink}>Contato</a></li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
