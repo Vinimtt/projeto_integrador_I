2f4aa3db4f3dd24e2443b56fd7949359cdce5e3d
pablo felipe <pabloletiane@gmail.com>
2025-10-21 15:44:31 -0300

    Adiciona componente ImageCarousel e atualiza NavBar para incluir logo. Atualiza dependências no package.json e package-lock.json. Adiciona imagens de exemplo ao diretório público.

--- projeto/src/app/page.js
+++ projeto/src/app/page.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import ImageCarousel from '../components/ImageCarousel' 
 
 export default function Home() {
   return (
     <>
-    <NavBar/>
-    
-    
+      <NavBar/>
+      <ImageCarousel /> 
     </>
   );
 }
--- projeto/src/components/NavBar.jsx
+++ projeto/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import Link from 'next/link';
 import styles from './Navbar.module.css';
+import Image from 'next/image';
 
 const NavBar = () => {
   return (
@@ -8,7 +9,15 @@ const NavBar = () => {
       
       <nav className={styles.navbar}>
         <div className={styles.navbarContainer}>
-          <a href="#" className={styles.navbarLogo}>MyLogo</a>
+          <Link href="/" className={styles.navbarLogo}>
+          <Image 
+            src="/images/logo.png"   
+            alt="Logo da Empresa"    
+            width={70}          
+            height={60} 
+            priority                 
+          />
+        </Link>
           <ul className={styles.navbarMenu}>
             <li className={styles.navbarItem}><a href="#" className={styles.navbarLink}>Início</a></li>
             <li className={styles.navbarItem}><a href="#" className={styles.navbarLink}>Quem somos?</a></li>
