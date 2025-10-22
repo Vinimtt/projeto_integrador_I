aa69284fd604838eb7ab9317a9a7d4886c121e4b
Vinícius Mergulhão <113155890+Vinimtt@users.noreply.github.com>
2025-10-22 00:51:17 +0000

    add: adiciona favicon e metadados no layout

--- projeto/src/app/page.js
+++ projeto/src/app/page.js
@@ -4,11 +4,10 @@ import Head from 'next/head';
 import NavBar from '../components/NavBar';
 import InicioSection from '../components/InicioSection';
 import QuemSomosSection from '../components/QuemSomosSection';
-<<<<<<< HEAD
 import ServicosSection from '../components/ServicosSection';
-=======
 import ProjectCarousel from '../components/ProjectCarousel';
->>>>>>> fc43b7777597124711b643bcd793046bc6a41fd3
+
+
 
 export default function Home() {
   return (
@@ -16,14 +15,12 @@ export default function Home() {
       <Head>
         <title>Empresa - Nossos Projetos</title>
         <meta name="description" content="Conheça nossos projetos e serviços" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <NavBar />
       <main>
         
         <InicioSection /> {}
-
         <QuemSomosSection/>
         <ServicosSection/>
         <ProjectCarousel />
