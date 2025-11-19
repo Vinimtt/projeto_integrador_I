81652fa14d875061bb822df343433ea1fc9f6506
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2025-11-19 02:52:19 +0000

    ajuste css dos projetos

--- projeto/src/app/galpao/page.jsx
+++ projeto/src/app/galpao/page.jsx
@@ -1,26 +1,46 @@
 // page.jsx
 import React from "react";
-import "./page.css"; // importa o CSS local
+import Link from "next/link";
+import "./page.css";
+import ContatoSection from "../../components/ContatoSection";
 
 export default function Page() {
   return (
-    <div className="project-container">
-      <h1 className="project-title">Título do Projeto</h1>
-
-      <div className="project-image-wrapper">
-        <img
-          src="/images/exemplo.jpg"
-          alt="Imagem do projeto"
-          className="project-image"
-        />
+    <>
+      {/* BOTÃO DE VOLTAR */}
+      <div className="back-button-wrapper">
+        <Link href="/" className="back-button">
+          ⬅ Voltar ao site
+        </Link>
       </div>
 
-      <p className="project-description">
-        Esta é uma descrição genérica do projeto. Substitua este texto pelo
-        conteúdo que desejar.
-      </p>
+      {/* CONTAINER DO PROJETO */}
+      <div className="project-container">
+        <h1 className="project-title">Centro Comercial & Lazer</h1>
 
-     
-    </div>
+        <div className="project-image-wrapper">
+          <img
+            src="/images/exemplo.jpg"
+            alt="Imagem do projeto"
+            className="project-image"
+          />
+        </div>
+
+        <p className="project-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat, mauris at dictum dictum, justo lorem facilisis nisl, vel maximus lectus risus vel ligula. Donec id semper lorem. Vivamus non aliquet orci.
+          <br /><br />
+          Suspendisse potenti. Vestibulum tincidunt sapien sit amet ligula facilisis placerat. Duis blandit, libero non rhoncus blandit, tellus lorem aliquam risus, eget elementum odio lorem a augue.
+          <br /><br />
+          Integer lacinia, nisl vel posuere gravida, eros orci sagittis ipsum, nec feugiat neque lectus id ipsum. Ut auctor odio eget mi ultricies, vel dictum erat volutpat. Aenean fringilla consectetur augue ac porta.
+          <br /><br />
+          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed faucibus ante quis arcu auctor posuere. Nullam eget orci ac libero scelerisque lobortis id in justo.
+          <br /><br />
+          Nam dignissim interdum nisl nec pharetra. Mauris interdum ligula in nisi pretium, nec convallis libero varius. Donec dapibus metus a ultrices commodo. Aliquam non mi odio.
+        </p>
+      </div>
+
+      {/* SEÇÃO QUE PREENCHE TODA A TELA */}
+      <ContatoSection />
+    </>
   );
 }
