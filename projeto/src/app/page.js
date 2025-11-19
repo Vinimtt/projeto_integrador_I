d9b83b0da68257901e25b5b39262c75267689d57
Pedroza1308 <pedroza1260@gmail.com>
2025-11-18 22:04:34 -0300

    adicionando_suspense_no_carrossel

--- projeto/src/app/page.js
+++ projeto/src/app/page.js
@@ -1,5 +1,5 @@
-// pages/index.js
-import React from 'react';
+// projeto/src/app/page.js
+import React, { Suspense } from 'react'; // 1. Importe o Suspense
 import Head from 'next/head';
 import NavBar from '../components/NavBar';
 import InicioSection from '../components/InicioSection';
@@ -22,7 +22,11 @@ export default function Home() {
         <InicioSection />
         <QuemSomosSection/>
         <ServicosSection/>
-        <ProjectCarousel />
+      
+        <Suspense fallback={<div>Carregando projetos...</div>}>
+          <ProjectCarousel />
+        </Suspense>
+
         <CuriosidadesSection />
         <ContatoSection />
       </main>
